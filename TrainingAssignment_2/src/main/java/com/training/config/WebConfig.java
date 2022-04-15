208d628a78f7360ac0f9c58d43f7955ea2f7b076
Soumyadeep Maji <soumyadeep.maji11@gmail.com>
2022-04-15 13:52:18 +0530

    A2

---
+++ TrainingAssignment_2/src/main/java/com/training/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.training.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebConfig implements WebApplicationInitializer {
+	
+	@Override
+	public void onStartup(ServletContext ctx) throws ServletException {
+		
+		AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+		webCtx.register(MvcConfig.class);
+		webCtx.setServletContext(ctx);
+		Dynamic servlet = ctx.addServlet("dispatcher", new DispatcherServlet(webCtx));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}
